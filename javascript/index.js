05bea0545c36d319736796fb93a371a59d8915b2
cinthia88 <bulacio.cinthia@gmail.com>
2022-11-13 22:51:23 -0300

    Subo primer cambio

---
+++ javascript/index.js
@@ -0,0 +1,27 @@
+
+
+/*
+let nombre = "Cinthia";
+
+console.log(typeof nombre)
+
+let numeroUno = 5;
+
+const numeroDos = 10;
+
+
+console.log(nombre)
+
+console.log(typeof numeroUno)
+console.log(numeroUno)
+*/
+
+let nombre = "Cinthia";
+let apellido = "Bulacio";
+let edad = 34;
+let mascota = "Perro";
+
+console.log(nombre)
+console.log(apellido)
+console.log(edad)
+console.log(mascota)
